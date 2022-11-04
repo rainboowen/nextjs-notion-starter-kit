5cd35f3e77d2dbe37ee3e6d40ed454adf9144e8c
rainboowen <rainboowen@gmail.com>
2022-11-04 13:42:48 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Index-6a14f7b6542649399dc7879d9ba87955',
+  rootNotionPageId: '6a14f7b6542649399dc7879d9ba87955',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -17,7 +17,7 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  WeChat: 'rainboowen',
+  wechat: 'rainboowen',
   github: 'rainboowen',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
